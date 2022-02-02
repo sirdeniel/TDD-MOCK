51aa0c1159b9c69f01e6382f5da0835205f4728c
jcagua <Jonathan.cagua@easymetering.com>
2022-02-02 17:53:10 -0500

    ADD: return_thru_ptr MOCK with pointer data

---
+++ iot_dev/src/hal/spi.h
@@ -0,0 +1,6 @@
+#ifndef SPI_H
+#define SPI_H
+typedef int EZM_SPI;
+
+int ezm_spi_read(EZM_SPI *my_obj,uint8_t reg_addr, uint8_t *data, uint32_t len);
+#endif // SPI_H
--- iot_dev/test/test_iis3dhhc_reg.c
+++ iot_dev/test/test_iis3dhhc_reg.c
@@ -2,13 +2,28 @@
 #include "unity.h"
 
 #include "iis3dhhc_reg.h"
-
+#include "mock_spi.h"
+EZM_SPI	app_sensor;
 void setUp(void)
 {
 }
 
 void tearDown(void)
 {
+}
+
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+    int32_t resp = 0;
+    resp = ezm_spi_read(&app_sensor,reg, bufp, len);
+    return resp;
+}
+
+static int32_t platform_write(void *handle, uint8_t reg, const uint8_t *bufp,
+                              uint16_t len)
+{
+    return 0;
 }
 
 void test_iis3dhhc_init_pointer_null(void)
@@ -19,3 +34,20 @@ void test_iis3dhhc_init_pointer_null(void)
     TEST_ASSERT_EQUAL_INT32(SENSOR_E_NULL_PTR,iis3dhhc_device_id_get(&dev_ctx, &whoamI));
 }
 
+void test_iis3dhhc_init_pointer_ok(void)
+{
+    stmdev_ctx_t dev_ctx = {0};
+    static uint8_t whoamI = 20;
+    static uint8_t SENSOR_BUS;
+    uint8_t reg_whoamI_mock = IIS3DHHC_WHO_AM_I;
+    uint8_t whoamI_mock = IIS3DHHC_ID;
+    dev_ctx.read_reg = platform_read;
+    dev_ctx.write_reg = platform_write;
+    dev_ctx.handle = &SENSOR_BUS;
+
+    ezm_spi_read_ExpectAndReturn(&app_sensor,reg_whoamI_mock,&whoamI_mock,1,SENSOR_OK);//&app_sensor,IIS3DHHC_WHO_AM_I,&whoamI_mock,1,0X0f);
+    ezm_spi_read_IgnoreArg_data();
+    ezm_spi_read_ReturnThruPtr_data(&whoamI_mock);
+    TEST_ASSERT_EQUAL_INT32(SENSOR_OK,iis3dhhc_device_id_get(&dev_ctx, &whoamI));
+    TEST_ASSERT_EQUAL_HEX(IIS3DHHC_ID,whoamI);
+}
