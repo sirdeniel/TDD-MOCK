2994594cfa526794fe970bdf2fd82fd75e0f54a5
jcagua <Jonathan.cagua@easymetering.com>
2022-02-02 08:08:36 -0500

    ADD: driver iis3dhhc_reg

--- iot_dev/src/temperatura.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEMPERATURA_H
-#define TEMPERATURA_H
-#include <stdbool.h>
-
-bool is_temperature_over_limit (void);
-void temperature_initialize (void);
-
-#endif // TEMPERATURA_H
---
+++ iot_dev/test/test_iis3dhhc_reg.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "iis3dhhc_reg.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_iis3dhhc_reg_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement iis3dhhc_reg");
+}
+
+#endif // TEST
