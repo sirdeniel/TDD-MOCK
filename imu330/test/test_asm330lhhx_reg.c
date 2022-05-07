4fe73c899ccc86688d5a52c03282fbe591e5a6c5
Daniel <sirdeniel3@gmail.com>
2022-05-07 15:30:30 -0500

    ADD: ceedling module:create[asm330lhhx_reg]

---
+++ imu330/test/test_asm330lhhx_reg.c
@@ -0,0 +1,20 @@
+#ifdef TEST
+
+#include "unity.h"
+
+#include "asm330lhhx_reg.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_asm330lhhx_reg_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement asm330lhhx_reg");
+}
+
+#endif // TEST
