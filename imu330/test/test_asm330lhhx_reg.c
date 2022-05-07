e0a3b72360f5863ed47ad9e6bbdd104caec0c770
Daniel <sirdeniel3@gmail.com>
2022-05-07 15:33:51 -0500

    ERR: unity fails because lib does not check null

--- imu330/test/test_asm330lhhx_reg.c
+++ imu330/test/test_asm330lhhx_reg.c
@@ -1,8 +1,28 @@
-#ifdef TEST
+// #ifdef TEST
 
 #include "unity.h"
 
 #include "asm330lhhx_reg.h"
+#include "mock_spi.h"
+#include "sensor_def.h"
+
+EZM_SPI	app_sensor;
+
+static int32_t platform_read(void *handle, uint8_t reg, uint8_t *bufp,
+                             uint16_t len)
+{
+    int32_t resp = 0;
+    resp = ezm_spi_read(&app_sensor,reg, bufp, len);
+    return resp;
+}
+
+static int32_t platform_write(void *handle, uint8_t reg, uint8_t *bufp,
+                              uint16_t len)
+{
+    int32_t resp = 0;
+    resp = ezm_spi_write(&app_sensor,reg, bufp, len);
+    return resp;
+}
 
 void setUp(void)
 {
@@ -12,9 +32,15 @@ void tearDown(void)
 {
 }
 
-void test_asm330lhhx_reg_NeedToImplement(void)
+void test_asm330lhhx_init(void)
 {
-    TEST_IGNORE_MESSAGE("Need to Implement asm330lhhx_reg");
+    stmdev_ctx_t dev_ctx;
+    // dev_ctx.write_reg = platform_write;
+    // dev_ctx.read_reg = platform_read;
+    static uint8_t whoamI;
+    whoamI = 0;
+    // TEST_IGNORE_MESSAGE("Need to Implement asm330lhhx_reg");
+    TEST_ASSERT_EQUAL_INT32(SENSOR_E_NULL_PTR,asm330lhhx_device_id_get(&dev_ctx, &whoamI));
 }
 
-#endif // TEST
+// #endif // TEST
